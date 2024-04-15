2bdc443ea541721b69df346cfc9a959b8810af39
Eden <47210353+edoob9@users.noreply.github.com>
2024-04-15 13:45:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,23 +14,61 @@ st.header("Music : In-vehicle music and video player")
 st.subheader("Let's start")
 st.video('./CoopernautIntro.mp4')
 
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
+def autoplay_audio(wav_file, is_auto = True):
+    with open(wav_file, "rb") as f:
         data = f.read()
         b64 = base64.b64encode(data).decode()
-        audio_html = f"""
-            <audio id="audio" controls autoplay>
-                <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-                Your browser does not support the audio element.
-            </audio>
-            <script>
-                window.onload = function() {{
-                    var audio = document.getElementById('audio');
-                    audio.play(controls=False);
-                }};
-            </script>
-        """
-        st.markdown(audio_html, unsafe_allow_html=True)
+        if is_auto:
+            md = f"""
+                <audio controls autoplay="true">
+                <source src="data:audio/mp3;base64,{b64}" type="audio/wav">
+                </audio>
+                """
+        else:
+            md = f"""
+                <audio controls autoplay="false">
+                <source src="data:audio/mp3;base64,{b64}" type="audio/wav">
+                </audio>
+                """
+
+    return md
+
+def autoplay_video(video_file, is_auto = True):
+    with open(video_file, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        if is_auto:
+            md = f"""
+                <video controls width=320 height=240 autoplay>
+                <source src="data:video/mp4;base64,{b64}">
+                </video>
+                """
+        else:
+            md = f"""
+                <video controls width=320 height=240 autoplay muted>
+                <source src="data:video/mp4;base64,{b64}">
+                </video>
+                """
+    return md
+
+
+# def autoplay_audio(file_path: str):
+#     with open(file_path, "rb") as f:
+#         data = f.read()
+#         b64 = base64.b64encode(data).decode()
+#         audio_html = f"""
+#             <audio id="audio" controls autoplay>
+#                 <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+#                 Your browser does not support the audio element.
+#             </audio>
+#             <script>
+#                 window.onload = function() {{
+#                     var audio = document.getElementById('audio');
+#                     audio.play(controls=False);
+#                 }};
+#             </script>
+#         """
+#         st.markdown(audio_html, unsafe_allow_html=True)
 
         # md = f"""
         #     <audio controls autoplay="true">
