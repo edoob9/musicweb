6e5d989fb004d37eefe38828bcd1a9b662e53955
Eden <47210353+edoob9@users.noreply.github.com>
2024-04-15 15:52:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,40 @@ st.set_page_config(
     layout = "wide",
 )
 
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        audio_html = f"""
+            <audio id="audio" controls autoplay>
+                <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+                Your browser does not support the audio element.
+            </audio>
+            <script>
+                window.onload = function() {{
+                    var audio = document.getElementById('audio');
+                    audio.play(controls=False);
+                }};
+            </script>
+        """
+        st.markdown(audio_html, unsafe_allow_html=True)
+
+        # md = f"""
+        #     <audio controls autoplay="true">
+        #     <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+        #     </audio>
+        #     """
+        # st.markdown(
+        #     md,
+        #     unsafe_allow_html=True,
+        # )
+
+
+st.write("# Audio Playing List!")
+autoplay_audio("./music/classic.mp3")
+
+
 st.header("Music : In-vehicle music and video player")
 st.subheader("Let's start")
 st.video('~/video/CoopernautIntro.mp4')
@@ -57,38 +91,6 @@ st.video('~/video/CoopernautIntro.mp4')
 #                 """
 #     return md
 
-
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        audio_html = f"""
-            <audio id="audio" controls autoplay>
-                <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-                Your browser does not support the audio element.
-            </audio>
-            <script>
-                window.onload = function() {{
-                    var audio = document.getElementById('audio');
-                    audio.play(controls=False);
-                }};
-            </script>
-        """
-        st.markdown(audio_html, unsafe_allow_html=True)
-
-        # md = f"""
-        #     <audio controls autoplay="true">
-        #     <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-        #     </audio>
-        #     """
-        # st.markdown(
-        #     md,
-        #     unsafe_allow_html=True,
-        # )
-
-
-st.write("# Audio Playing List!")
-autoplay_audio("./music/classic.mp3")
 
 #audio_file = open('/Users/eden/Downloads/test.mp3', 'rb')
 #st.audio(audio_file.read(), format='audio/mp3')
