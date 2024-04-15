8df0453d2c195b9650849f5e7d4554860b164f22
Eden <47210353+edoob9@users.noreply.github.com>
2024-04-15 15:28:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def autoplay_audio(file_path: str):
 
 
 st.write("# Auto-playing Audio!")
-autoplay_audio("./test.mp3")
+autoplay_audio("./classic.mp3")
 
 #audio_file = open('/Users/eden/Downloads/test.mp3', 'rb')
 #st.audio(audio_file.read(), format='audio/mp3')
